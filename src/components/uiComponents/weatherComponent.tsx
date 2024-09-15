c6a4977b5e9b98d5275de016a2fcc1a1139f3629
adityakr1403 <adityakr1403@gmail.com>
2024-09-15 15:02:04 +0530

    initial commit

--- src/components/uiComponents/weatherComponent.tsx
+++ src/components/uiComponents/weatherComponent.tsx
@@ -21,8 +21,8 @@ const WeatherComponent = () => {
         try {
             fetch('https://ipapi.co/json/').then((response) => response.json()).then((data) => {
                 setGeoLocation({
-                    latitude: data.lat,
-                    longitude: data.lon
+                    latitude: data.latitude,
+                    longitude: data.longitude
                 });
             });
         } catch (e) {
